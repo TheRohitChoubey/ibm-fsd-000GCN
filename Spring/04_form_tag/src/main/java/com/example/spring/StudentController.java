e4e05eee7fb5439fb49b581e3196f2141c1b3d48
TheRohitChoubey <rohitc.choubey.com>
2019-09-08 22:36:38 +0530

    New changes

--- Spring/04_form_tag/src/main/java/com/example/spring/StudentController.java
+++ Spring/04_form_tag/src/main/java/com/example/spring/StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student")
@@ -20,40 +20,48 @@ public class StudentController {
 	public String viewPage() {
 		return "index";
 	}
-	
+
 	@GetMapping("/formView")
 	public String processPage(Model theModel) {
 		Student student = new Student();
-		theModel.addAttribute("student",student);
+		theModel.addAttribute("student", student);
 		return "student-process";
 	}
-	
+
 	@PostMapping("/processForm")
-	public String processForm(@ModelAttribute("student") Student theStudent,Model theModel) {
-		theModel.addAttribute("tempStudent",theStudent);
+	public String processForm(@ModelAttribute("student") Student theStudent, Model theModel) {
+		theModel.addAttribute("tempStudent", theStudent);
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		StudentDao dao = context.getBean("eDao", StudentDao.class);
 		dao.createStudent(theStudent);
 		return "student-process";
 	}
-	
+
+	@PostMapping("/searchByName")
+	public String searchByName(@ModelAttribute("student") Student theStudent, Model theModel) {
+		List<Map<String, Object>> studentsList;
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		StudentDao dao = context.getBean("eDao", StudentDao.class);
+		studentsList = dao.searchStudent(theStudent);
+		theModel.addAttribute("foundStudent", studentsList);
+		return "student-process";
+	}
+
 	@GetMapping("/getData")
 	public String getDetails(Model theModel) {
-		 List<Map<String, Object>> studentsList;
+		List<Map<String, Object>> studentsList;
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		StudentDao dao = context.getBean("eDao", StudentDao.class);
 		studentsList = dao.getStudents();
-		theModel.addAttribute("studentList",studentsList);
+		theModel.addAttribute("studentList", studentsList);
 		return "student-detail";
 	}
-	
-	@PostMapping("/getName")
-	public String getByName(@ModelAttribute("student") Student theStudent,Model theModel) {
-		 List<Map<String, Object>> studentsList;
+
+	@GetMapping("/getName")
+	public String getByName(@RequestParam String order) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		StudentDao dao = context.getBean("eDao", StudentDao.class);
-		studentsList = dao.getStudents();
-		theModel.addAttribute("studentList",studentsList);
+		dao.deleteStudent(order);
 		return "student-detail";
 	}
 }
--- Spring/04_form_tag/src/main/java/com/example/spring/StudentDao.java
+++ Spring/04_form_tag/src/main/java/com/example/spring/StudentDao.java
@@ -7,19 +7,16 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
-@Component(value="eDao")
+@Component(value = "eDao")
 public class StudentDao {
-	
+
 	@Autowired
-	 JtemplateClass jdbcTemplate;
-	
-	
+	JtemplateClass jdbcTemplate;
+
 	public JtemplateClass getJdbcTemplate() {
 		return jdbcTemplate;
 	}
-	
-	
+
 	public void setJdbcTemplate(JtemplateClass jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
@@ -30,27 +27,54 @@ public class StudentDao {
 		jdbcTemplate.update(query, obj);
 		System.out.println("done");
 	}
-	
-	public List getStudents()	{
-		String query="select * from student";
-        
-        List<Map<String, Object>> students =  jdbcTemplate.queryForList(query);
-        if (students!=null && !students.isEmpty()) {
-            
-            for (Map<String, Object> student : students) {
-                 
-                for (Iterator<Map.Entry<String, Object>> it = student.entrySet().iterator(); it.hasNext();) {
-                    Map.Entry<String, Object> entry = it.next();
-                    String key = entry.getKey();
-                    Object value = entry.getValue();
-                    System.out.println(key + " = " + value);
-                }
-                System.out.println();
-            }
-        }
+
+	public void deleteStudent(String order) {
+		String query = "delete from student where id = ?";
+		Object obj[] = { (Integer.parseInt(order)) };
+		jdbcTemplate.update(query, obj);
+		System.out.println("done");
+	}
+
+	public List searchStudent(Student stud) {
+		String query = "select * from student where firstname like '%"+stud.getFirstName()+"%'";
+		List<Map<String, Object>> students = jdbcTemplate.queryForList(query);
+		
+		if (students != null && !students.isEmpty()) {
+
+			for (Map<String, Object> student : students) {
+
+				for (Iterator<Map.Entry<String, Object>> it = student.entrySet().iterator(); it.hasNext();) {
+					Map.Entry<String, Object> entry = it.next();
+					String key = entry.getKey();
+					Object value = entry.getValue();
+					System.out.println(key + " = " + value);
+				}
+				System.out.println();
+			}
+		}
 		System.out.println("done");
 		return students;
 	}
 
+	public List getStudents() {
+		String query = "select * from student";
+
+		List<Map<String, Object>> students = jdbcTemplate.queryForList(query);
+		if (students != null && !students.isEmpty()) {
+
+			for (Map<String, Object> student : students) {
+
+				for (Iterator<Map.Entry<String, Object>> it = student.entrySet().iterator(); it.hasNext();) {
+					Map.Entry<String, Object> entry = it.next();
+					String key = entry.getKey();
+					Object value = entry.getValue();
+					System.out.println(key + " = " + value);
+				}
+				System.out.println();
+			}
+		}
+		System.out.println("done");
+		return students;
+	}
 
 }
